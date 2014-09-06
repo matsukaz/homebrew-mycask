e11d34a497a1e92ac592b01cce019f326de52678
matsukaz <matsukaz@gmail.com>
2014-09-06 15:42:53 +0900

    Fixed Window Wrangler

--- Casks/window-wrangler.rb
+++ Casks/window-wrangler.rb
@@ -3,5 +3,5 @@ class WindowWrangler < Cask
   homepage 'http://mindflow.dk/software/windowwrangler/'
   version '1.0.1'
   sha256 '889efeccd1ef6b75e11a7050e18317681e3a4c118825bc0b52e892cb017439c7'
-  link 'Freeplane.app'
+  link 'Window Wrangler.app'
 end
