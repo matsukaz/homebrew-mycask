401b7250263f60e9b03794d93bf07e61b5801538
matsukaz <matsukaz@gmail.com>
2014-09-06 15:26:30 +0900

    Fixed freeplane cask

--- Casks/freeplane.rb
+++ Casks/freeplane.rb
@@ -1,7 +1,7 @@
 class Freeplane < Cask
-  url 'http://downloads.sourceforge.net/project/freeplane/freeplane%20stable/freeplane_app-1.3.11.dmg'
-  homepage 'http://sourceforge.net/projects/freeplane/'
+  url 'https://downloads.sourceforge.net/sourceforge/freeplane/freeplane_app-1.3.11.dmg'
+  homepage 'http://freeplane.sourceforge.net/'
   version '1.3.11'
-  md5 '71A522FEE4BABF2843270940D8AF1B7F'
+  sha256 :no_check
   link 'Freeplane.app'
 end
