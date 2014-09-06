6de19f66e25886bd82952a87ba7867014683e27f
matsukaz <matsukaz@gmail.com>
2014-09-06 15:39:05 +0900

    Added Window Wrangler

--- Casks/freeplane.rb
+++ Casks/freeplane.rb
@@ -2,6 +2,6 @@ class Freeplane < Cask
   url 'https://downloads.sourceforge.net/sourceforge/freeplane/freeplane_app-1.3.11.dmg'
   homepage 'http://freeplane.sourceforge.net/'
   version '1.3.11'
-  sha256 :no_check
+  sha256 '336d4785cadcdb049453c9fcf7d80b0fad21f74d9b96132a74039e4cd5897671'
   link 'Freeplane.app'
 end
---
+++ Casks/window-wrangler.rb
@@ -0,0 +1,7 @@
+class WindowWrangler < Cask
+  url 'http://mindflow.dk/software/windowwrangler/windowwrangler.dmg'
+  homepage 'http://mindflow.dk/software/windowwrangler/'
+  version '1.0.1'
+  sha256 '889efeccd1ef6b75e11a7050e18317681e3a4c118825bc0b52e892cb017439c7'
+  link 'Freeplane.app'
+end
